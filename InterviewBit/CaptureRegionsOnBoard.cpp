68ebd55b0d5507a5bda4830ad204a3d6eb8e8eb4
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-07-26 17:54:37 +0530

    imp question

--- InterviewBit/CaptureRegionsOnBoard.cpp
+++ InterviewBit/CaptureRegionsOnBoard.cpp
@@ -1,45 +1,41 @@
-// Source : https://www.geeksforgeeks.org/given-matrix-o-x-replace-o-x-surrounded-x/
-
-void dfs(vector<vector<char>>& A, int i, int j) {
-    if(i >= A.size() || i < 0 || j >= A[0].size() || j < 0 || A[i][j] != '-')
+void dfs(vector<vector<char>> &A, int i, int j) {
+    if(i < 0 || i >= A.size() || j < 0 || j >= A[0].size() 
+    || A[i][j] != 'O')
         return;
-    A[i][j] = '0';
-    dfs(A, i+1, j);
-    dfs(A, i-1, j);
-    dfs(A, i, j+1);
-    dfs(A, i, j-1);
+    A[i][j] = 'Y';
+    dfs(A, i - 1, j);   dfs(A, i, j - 1);   dfs(A, i + 1, j);
+    dfs(A, i, j + 1);
 }
 
 void Solution::solve(vector<vector<char> > &A) {
-    // Do not write main() function.
-    // Do not read input, instead use the arguments to the function.
-    // Do not print the output, instead return values as specified
-    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
-    // Step - 1
-    for(int i=0; i<A.size(); i++) {
-        for(int j=0; j<A[0].size(); j++) {
-            if(A[i][j] == '0')
-                A[i][j] = '-';
+    int m = A.size(), n = A[0].size();
+    for(int i = 0; i < m; i++) {
+        if(A[i][0] == 'O') {
+            dfs(A, i, 0);
+        }
+        if(A[i][n - 1] == 'O') {
+            dfs(A, i, n - 1);
         }
     }
-    // Step - 2
-    for(int i=0; i<A.size(); i++) {
-        dfs(A, i, 0);
+    
+    for(int i = 0; i < n; i++) {
+        if(A[0][i] == 'O') {
+            dfs(A, 0, i);
+        }
+        if(A[m - 1][i] == 'O') {
+            dfs(A, m - 1, i);
+        }
     }
-    for(int j=0; j<A[0].size(); j++) {
-        dfs(A, 0, j);
-    }
-    for(int i=0; i<A.size(); i++) {
-        dfs(A, i, A.size() - 1);
-    }
-    for(int j=0; j<A[0].size(); j++) {
-        dfs(A, A[0].size() - 1, j);
-    }
-    // Step - 3
-    for(int i=0; i<A.size(); i++) {
-        for(int j=0; j<A[0].size(); j++) {
-            if(A[i][j] == '-')
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(A[i][j] == 'O')
                 A[i][j] = 'X';
+        }
+    }
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(A[i][j] == 'Y')
+                A[i][j] = 'O';
         }
     }
 }
