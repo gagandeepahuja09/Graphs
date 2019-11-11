9fedd053f798a66987a3f5d9a58aa9f93b7a255e
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-11-11 23:33:09 +0530

    easy

--- LeetCode/Similar String Groups.cpp
+++ LeetCode/Similar String Groups.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    vector<int> parent;
     int clusters;
-    
-    void uni(int i, int j) {
-        int u = find(i), v = find(j);
-        if(u != v) {
-            parent[u] = v;
-            clusters--;
+    vector<int> parent;
+    bool isSimilar(string s, string t) {
+        int cnt = 0;
+        if(s.size() != t.size())    return false;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] != t[i])    cnt++;
         }
+        return (cnt <= 2);
     }
     
     int find(int u) {
-        if(parent[u] == u)
-            return u;
-        return parent[u] = find(parent[u]);
+        if(u != parent[u]) {
+            parent[u] = find(parent[u]);
+        }
+        return parent[u];
     }
     
-    bool isSimilar(string u, string v) {
-        int n = 0;
-        for(int i = 0; i < u.size(); i++) {
-            if(u[i] != v[i] && ++n > 2)
-                return false;
+    void uni(int u, int v) {
+        int pu = find(u), pv = find(v);
+        if(pu != pv) {
+            parent[pu] = pv;
+            clusters--;
         }
-        return true;
     }
     
     int numSimilarGroups(vector<string>& A) {
         int n = A.size();
         clusters = n;
+        parent.resize(n);
         for(int i = 0; i < n; i++)
-            parent.push_back(i);
+            parent[i] = i;
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
                 if(isSimilar(A[i], A[j])) {
                     uni(i, j);
+                    // cout << "Yes";
                 }
-            }    
+            }
         }
         return clusters;
     }
