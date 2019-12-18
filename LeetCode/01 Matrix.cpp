4e03c6cd0d0e3b4c73e82f2becfb9a12a7384902
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-12-18 11:33:36 +0530

    easy

--- LeetCode/01 Matrix.cpp
+++ LeetCode/01 Matrix.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& A) {
-        queue<pair<int, pair<int, int>>> q;
-        vector<vector<int>> ret(A.size(), vector<int>(A[0].size()));
-        for(int i = 0; i < A.size(); i++) {
-            for(int j = 0; j < A[0].size(); j++) {
-                if(A[i][j] == 0) {
-                    ret[i][j] = 0;
-                    q.push({ i, { j, 0 }});
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<vector<int>> q;
+        for(int i = 0; i < mat.size(); i++) {
+            for(int j = 0; j < mat[0].size(); j++) {
+                if(mat[i][j] == 0) {
+                    q.push({ i, j, 0 });
+                }
+                else {
+                    mat[i][j] = -2;
                 }
             }
         }
-        vector<int> dx = { -1,  0, 1, 0 };
-        vector<int> dy = {  0, -1, 0, 1 };
+        vector<int> dx  = { -1, 0, 1, 0};
+        vector<int> dy  = { 0, -1, 0, 1};
         while(!q.empty()) {
-            auto p = q.front();
+            auto f = q.front();
             q.pop();
-            int x = p.first, y = p.second.first, steps = p.second.second;
-            A[x][y] = -1;
-            ret[x][y] = steps;
+            int x = f[0], y = f[1], steps = f[2];
+            if(x < 0 || x >= mat.size() || y < 0 || y >= mat[0].size() || mat[x][y] == -1 || mat[x][y] > 0) {
+                continue;
+            }
             for(int i = 0; i < 4; i++) {
                 int cx = x + dx[i], cy = y + dy[i];
-                if(cx >= 0 && cx < A.size() && cy >= 0 && cy < A[0].size() && A[cx][cy] == 1) {
-                    A[cx][cy] = -1;
-                    q.push({ cx, { cy, steps + 1 }});
-                }
+                q.push({ cx, cy, steps + 1});
+            }
+            if(mat[x][y] == 0) {
+                mat[x][y] = -1;
+            }
+            if(mat[x][y] == -2) {
+                mat[x][y] = steps;
             }
         }
-        return ret;
+        for(int i = 0; i < mat.size(); i++) {
+            for(int j = 0; j < mat[0].size(); j++) {
+                if(mat[i][j] == -1)
+                    mat[i][j] = 0; 
+            }
+        }
+        return mat;
     }
 };
