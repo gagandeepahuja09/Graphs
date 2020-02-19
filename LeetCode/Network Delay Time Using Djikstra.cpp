ddad5971c5a81f7781955f43b8b51962bbfadc3f
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-02-19 13:41:05 +0530

    revised

--- LeetCode/Network Delay Time Using Djikstra.cpp
+++ LeetCode/Network Delay Time Using Djikstra.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    vector<int> dijkstra(vector<vector<pair<int, int>>>& g, int src) {
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        vector<int> dist(N + 1, INT_MAX);
+        dist[0] = 0;
+        vector<vector<pair<int, int>>> adj(N + 1);
+        for(auto t : times) {
+            adj[t[0]].push_back({ t[1], t[2] });    
+        }
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({ 0, src });
-        int n = g.size();
-        vector<int> ret(n, INT_MAX);
+        pq.push({ 0, K });
         while(!pq.empty()) {
-            auto t = pq.top();
+            int node = pq.top().second, currDist = pq.top().first;
             pq.pop();
-            int w = t.first, v = t.second;
-            if(ret[v] != INT_MAX)
+            if(dist[node] != INT_MAX)
                 continue;
-            ret[v] = w;
-            for(auto p : g[v]) {
-                pq.push({ w + p.second, p.first });
+            dist[node] = currDist;
+            for(auto p : adj[node]) {
+                pq.push({ currDist + p.second, p.first });
             }
         }
-        return ret;
-    }
-    
-    int networkDelayTime(vector<vector<int>>& t, int N, int K) {
-        vector<vector<pair<int, int>>> g(N);
-        for(auto e : t) {
-            g[e[0] - 1].push_back({ e[1] - 1, e[2] });
-        }
-        vector<int> ret = dijkstra(g, K - 1);
-        int mx = 0;
-        for(int i = 0; i < ret.size(); i++)
-            mx = max(mx, ret[i]);
-        return mx == INT_MAX ? -1 : mx;
+        int delay = *max_element(dist.begin(), dist.end());
+        return delay == INT_MAX ? -1 : delay;
     }
 };
