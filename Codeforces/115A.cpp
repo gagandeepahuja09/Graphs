13f7f2fb579c29b3704627b05bfa447e21047d17
Parush <69762039+parush2305@users.noreply.github.com>
2020-10-01 21:38:05 +0530

    Update 115A.cpp
    added comment

--- Codeforces/115A.cpp
+++ Codeforces/115A.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n;
-    cin >> n;
+    cin >> n; //input
     vector<vector<int>> g(n + 1);
     vector<int> ind(n + 1, 0);
     for(int i = 0; i < n; i++) {
